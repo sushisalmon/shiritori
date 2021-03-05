4778beff343c37c226128ed828a20a1a741cdf95
sushisalmon <sekiwataru3208@gmail.com>
2021-03-05 09:49:25 +0900

    fist shiritori

---
+++ shiritori.py
@@ -0,0 +1,28 @@
+def shiritori(x, y, i, list1, list2):
+    if i == x :
+
+
+
+
+
+f_name = input("Input file name:")
+
+fp = open(f_name ,"r")
+
+f_letter = fp.read()
+
+all_list = f_letter.split("\n")
+
+try:
+    use_num = int(all_list[0])
+
+except ValueError:
+    print("１行目はしりとりで使う文字列の数にしてください！")
+
+
+for pick in all_list:
+    if pick >= "0" and pick "9":
+        ans_list = list()
+        str_num = a
+
+        use_num = int(pick)
